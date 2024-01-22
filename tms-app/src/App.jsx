4638ecb8481c5c9cd13fe0cfbefae59ea8d585c0
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-01-22 15:35:45 -0500

    Add leaflet component

--- tms-app/src/App.jsx
+++ tms-app/src/App.jsx
@@ -1,31 +1,12 @@
-import React, {useState, useEffect} from "react";
-
+import React from "react";
+import LeafletComponent from "./leaflet_stuff";
 
 function App() {
-  const [data, setData] = useState([{}])
-
-  useEffect(() => {
-    fetch("http://localhost:5000/first-test").then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
-  }, [])
-
   return (
     <div>
-      {(typeof	data.test === "undefined") ? (
-        <p>loading...</p>
-      ) : (
-        data.test.map((test, i) => (
-          <p key={i}>{test}</p>
-        ))
-      )}
+      <LeafletComponent />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
