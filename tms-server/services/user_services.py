4434031abcdfc0b5f47a78d2a0fa30747f8a27d7
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-04-22 21:45:31 -0400

    Add teardown for db connection

--- tms-server/services/user_services.py
+++ tms-server/services/user_services.py
@@ -1,13 +1,18 @@
 from flask import session, g
 import sqlite3 as sql
 
+# Close db connection after every request
+@app.teardown_appcontext
+def close_db(exception):
+    if "db" in g:
+        g.db.close()
+
+
 # ADMIN (permision to everything, managing accounts, system config, performance)
 # EDIT DATABSE
 # ADD USERS
 # DELETE ACCOUNTS
 # ACCESS TO LOGS
-
-
 
 
 """ ORGANIZE BELOW """
