7a372866fe6c5b63c032cb9df4ca73dc59b2b915
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-04-22 21:00:35 -0400

    Modify login_required decorator

--- tms-server/utils/decorators.py
+++ tms-server/utils/decorators.py
@@ -3,12 +3,19 @@ from functools import wraps
 
 
 # Define login_required decorator, redirects to login if not logged in
-""" Change per user """
-def login_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if session.get("user_id"):
-            return f(*args, **kwargs)
-        else:
-            return redirect("/login")
-    return decorated_function
+def login_required(role_required):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if session.get("user_id"):
+                # Check if user's role matches role required
+                if check_user_role(session.get("user_id"), role_required): # TODO: Make this function
+                    return f(*args, **kwargs)
+                else:
+                    # TODO: Make this redirect
+                    return redirect("/unauthorized")
+            else:
+                # TODO: Make this redirect
+                return redirect("/login")
+        return decorated_function
+    return decorator
