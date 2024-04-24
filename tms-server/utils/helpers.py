0bd1135664e1509bfd81e5ed378139bc83a08ec1
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-04-23 21:25:19 -0400

    Modify to get user info from g

--- tms-server/utils/helpers.py
+++ tms-server/utils/helpers.py
@@ -1,32 +1,17 @@
-from flask import session, g
-import sqlite3 as sql
-from ..services.user_services import close_db
-
-
-# Establish db connection, returns db connection from g
-def get_db():
-    # Create a connection if doesn't exist
-    if "db" not in g:
-        g.db = sql.connect("tms_database.db")
-
-        # Set queries to return Row objects
-        g.db.row_factory = sql.Row
-    return g.db
-
+from flask import g
 
 # Compares user's role with a set role, returns true or false TODO: test
 def check_user_role(user_id, role):
     try:
-        db = get_db()
-        res = db.execute("SELECT role FROM users WHERE user_id = ?", (user_id,))
-        row = res.fetchone()
+        # Get user information TODO: check if this actually gets from g, test if no user
+        user = g.user
 
         # User_id not found
-        if not row:
-            raise ValueError("User Id not found")
+        if not g.user:
+            raise ValueError("User not found")
 
         # Role matches
-        if row["role"] == role:
+        if user["role"] == role:
             return True
 
         # Role doesn't match
@@ -34,7 +19,3 @@ def check_user_role(user_id, role):
 
     except Exception as e:
         return str(e)  # TODO: Handle errors (consider what it returns)
-
-    finally:
-        # Close connection regardless of outcome
-        close_db()
