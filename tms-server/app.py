4425ca19cad47cbd30f1b200a11805971b25a8ed
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-04-23 21:24:44 -0400

    Add blueprints

--- tms-server/app.py
+++ tms-server/app.py
@@ -1,8 +1,19 @@
-from flask import Flask
+from flask import Flask, g
+from routing.admin import admin_blueprint
 
 
 app = Flask(__name__)
 
+
+# Close db connection after every request
+@app.teardown_appcontext
+def close_db(exception):
+    if "db" in g:
+        g.db.close()
+
+
+# Register blueprints
+app.register_blueprint(admin_blueprint)
 
 if __name__ == "__main__":
     app.run(debug=True)
