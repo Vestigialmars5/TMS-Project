201c0eac1ccaf007b732cfbd3b4691dc3f94a3e7
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-04-28 17:42:42 -0400

    Add tests

--- tms-server/routing/admin.py
+++ tms-server/routing/admin.py
@@ -1,15 +1,14 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for
 
 admin_blueprint = Blueprint("admin", __name__, url_prefix="/admin")
 
 
-@admin_blueprint.route("/dashboard")
-def dashboard():
-    return 
+@admin_blueprint.route("/")
+def index():
+    return "<p>Hello, World!</p>"
+
 # ADMIN (permision to everything, managing accounts, system config, performance)
 # EDIT DATABSE
 # ADD USERS
 # DELETE ACCOUNTS
 # ACCESS TO LOGS
-
-
