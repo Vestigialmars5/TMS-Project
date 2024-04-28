ac243eccb02431b775f7d1f53f40ac671a7a129d
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-04-28 17:42:09 -0400

    Add test for db

--- tms-server/app.py
+++ tms-server/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, g
 from routing.admin import admin_blueprint
+from routing.auth import auth_blueprint
 
 
 app = Flask(__name__)
@@ -8,12 +9,16 @@ app = Flask(__name__)
 # Close db connection after every request
 @app.teardown_appcontext
 def close_db(exception):
-    if "db" in g:
+    db = g.pop("db", None)
+    if db is not None:
+        print("closing db connection")
         g.db.close()
 
 
 # Register blueprints
 app.register_blueprint(admin_blueprint)
+app.register_blueprint(auth_blueprint)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
