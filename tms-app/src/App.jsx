ae808ba828d9b24b322e7b83d3f75dd39abfd657
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-06-01 22:25:51 -0400

    Implement react router

--- tms-app/src/App.jsx
+++ tms-app/src/App.jsx
@@ -1,10 +1,35 @@
-import React from "react";
-import LeafletComponent from "./leaflet_stuff";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import NoPage from "./components/NoPage/NoPage";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [email, setEmail] = useState("");
+
   return (
-    <div>
-      <LeafletComponent />
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home
+                email={email}
+                loggedIn={loggedIn}
+                setLoggedIn={setLoggedIn}
+              />
+            }
+          />
+          <Route
+            path="/login"
+            element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />}
+          />
+          <Route path="*" element={<NoPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
