14eb7c4cca451bb8d8155e34d9612d5989448457
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-06-02 22:37:51 -0400

    Add TODO

--- tms-app/src/components/Login/LoginForm.jsx
+++ tms-app/src/components/Login/LoginForm.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+// TODO: Real-time validation
 const LoginForm = ({
   email,
   setEmail,
@@ -12,32 +13,34 @@ const LoginForm = ({
   return (
     <div className="mainContainer">
       <div className="titleContainer">Login</div>
-      <div className="inputContainer">
-        <input
-          value={email}
-          placeholder="Enter your email here"
-          onChange={(e) => setEmail(e.target.value)}
-          className="inputBox"
-        />
-        <label className="errorLabel">{emailError}</label>
-      </div>
-      <div className="inputContainer">
-        <input
-          value={password}
-          placeholder="Enter your password here"
-          onChange={(e) => setPassword(e.target.value)}
-          className="inputBox"
-        />
-        <label className="errorLabel">{passwordError}</label>
-      </div>
-      <div className="inputContainer">
-        <input
-          className="inputButton"
-          type="button"
-          onClick={handleLogin}
-          value="Log in"
-        />
-      </div>
+      <form>
+        <div className="inputContainer">
+          <input
+            value={email}
+            placeholder="Enter your email here"
+            onChange={(e) => setEmail(e.target.value)}
+            className="inputBox"
+          />
+          <label className="errorLabel">{emailError}</label>
+        </div>
+        <div className="inputContainer">
+          <input
+            value={password}
+            placeholder="Enter your password here"
+            onChange={(e) => setPassword(e.target.value)}
+            className="inputBox"
+          />
+          <label className="errorLabel">{passwordError}</label>
+        </div>
+        <div className="inputContainer">
+          <input
+            className="inputButton"
+            type="button"
+            onClick={handleLogin}
+            value="Log in"
+          />
+        </div>
+      </form>
     </div>
   );
 };
