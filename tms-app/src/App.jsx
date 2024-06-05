880b11941dfcb7a9a685446b4fe6e33d8bdbee71
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-06-05 13:29:00 -0400

    Update imports

--- tms-app/src/App.jsx
+++ tms-app/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Login from "./pages/Login/Login";
-import NoPage from "./pages/NoPage/NoPage";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import NoPage from "./pages/no-page/NoPage";
 import "./App.css";
 import { useEffect, useState } from "react";
 
