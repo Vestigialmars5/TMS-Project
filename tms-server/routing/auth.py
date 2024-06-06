34d9462eddb108a3419c8ffeafbe2c5ee53d9169
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-06-06 17:01:44 -0400

    Move register to admin
    Only admin has permission to register

--- tms-server/routing/auth.py
+++ tms-server/routing/auth.py
@@ -17,37 +17,7 @@ from db import get_db
 auth_blueprint = Blueprint("auth", __name__, url_prefix="/auth")
 
 # TODO: check auth libraries
-
-
-# TODO: check this, copied from flask's official website
-@auth_blueprint.route("/register", methods=("GET", "POST"))
-def register():
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
-        db = get_db()
-        error = None
-
-        if not username:
-            error = "Username is required."
-        elif not password:
-            error = "Password is required."
-
-        if error is None:
-            try:
-                db.execute(
-                    "INSERT INTO user (username, password) VALUES (?, ?)",
-                    (username, generate_password_hash(password)),
-                )
-                db.commit()
-            except db.IntegrityError:
-                error = f"User {username} is already registered."
-            else:
-                return redirect(url_for("auth.login"))
-
-        flash(error)
-
-    return render_template("auth/register.html")
+# TODO: Implement jwt
 
 
 @auth_blueprint.route("/login", methods=("GET", "POST"))
