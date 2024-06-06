39eca1152d3e040ebaa1a8d38fbeaf1675b9d9dd
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-06-06 02:13:39 -0400

    Add layout and fix PrivateRoute

--- tms-app/src/App.jsx
+++ tms-app/src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/layout/Layout";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import NoPage from "./pages/no-page/NoPage";
-import AdminDashboard from ".pages/dashboard/AdminDashboard";
+import AdminDashboard from "./pages/dashboard/admin/AdminDashboard";
 import PrivateRoute from "./components/private-route/PrivateRoute";
 import "./App.css";
 import { useEffect, useState } from "react";
@@ -15,22 +16,29 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <Home
-                email={email}
-                loggedIn={loggedIn}
-                setLoggedIn={setLoggedIn}
-              />
-            }
-          />
-          <Route
-            path="/login"
-            element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />}
-          />
-          <Route path="*" element={<NoPage />} />
-          <PrivateRoute path="/admin" authToken={authToken} role="admin" component={AdminDashboard} />
+          <Route path="/" element={<Layout />}>
+            <Route path="/home"
+              element={
+                <Home
+                  email={email}
+                  loggedIn={loggedIn}
+                  setLoggedIn={setLoggedIn}
+                />
+              }
+            />
+            <Route
+              path="/login"
+              element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />}
+            />
+            {/* Private routes for admin */}
+            <Route
+              path="/admin"
+              element={<PrivateRoute requiredRole="admin" />}
+            >
+              <Route path="/admin" element={<AdminDashboard/>} />
+            </Route>
+            <Route path="*" element={<NoPage />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
