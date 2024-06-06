e2d39075fcf16b5c708aef6677a0dcf3b95e1d7f
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-06-06 02:14:02 -0400

    Remove unnecesary things

--- tms-app/src/components/layout/Layout.jsx
+++ tms-app/src/components/layout/Layout.jsx
@@ -6,18 +6,8 @@ const Layout = () => {
   return (
     <div>
       <header>
-        <h1>My App</h1>
         <nav>
           <ul>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/about">About</a>
-            </li>
-            <li>
-              <a href="/contact">Contact</a>
-            </li>
             <li>
               <LogoutButton />
             </li>
