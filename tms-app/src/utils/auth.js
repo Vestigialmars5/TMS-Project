0d796c525744b549e6c555afd7aee5d924e36dea
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-06-06 17:00:45 -0400

    Add getUserRole function

--- tms-app/src/utils/auth.js
+++ tms-app/src/utils/auth.js
@@ -2,44 +2,53 @@ import { jwtDecode } from "jwt-decode";
 
 // Function to store JWT token in local storage
 export const storeToken = (token) => {
-    localStorage.setItem("token", token);
+  localStorage.setItem("token", token);
 };
 
 // Function to get JWT token from local storage
 export const getToken = () => {
-    return localStorage.getItem("token");
+  return localStorage.getItem("token"); // Null if no token
 };
 
 // Function to remove JWT token from local storage
-export const removeToken = (token) => {
-    localStorage.removeItem("token");
+export const removeToken = () => {
+  localStorage.removeItem("token");
 };
 
 // Function to decode JWT token and get user info
 export const decodeToken = (token) => {
-    try {
-        const decoded = jwtDecode(token);
-        return decoded;
-    } catch (error) {
-        return null;
-    }
+  try {
+    const decoded = jwtDecode(token);
+    return decoded;
+  } catch (error) {
+    return null;
+  }
 };
 
 // Function to check if user is authenticated
 export const isAuthenticated = () => {
-    const token = getToken();
+  const token = getToken();
   return token !== null;
 };
 
 // Function to authenticate user
 export const authenticateUser = (token) => {
-    if (token) {
-        storeToken(token);
-    }
+  if (token) {
+    storeToken(token);
+  }
 };
 
 export const isAuthorized = (requiredRole) => {
-    const token = getToken();
-    const role = token.role;
-    return role === requiredRole;
+  const token = getToken();
+  const role = getUserRole(token);
+  return role === requiredRole;
+};
+
+export const getUserRole = (token) => {
+  const decoded = decodeToken(token);
+  if (decoded !== null) {
+    return decoded.role;
+  } else {
+    return null;
+  }
 };
