9aebd2c51bccf54e23195ffea19c2516baa74d21
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-06-06 02:13:20 -0400

    Handle logout succesfully

--- tms-app/src/components/logout/LogoutButton.jsx
+++ tms-app/src/components/logout/LogoutButton.jsx
@@ -3,36 +3,38 @@ import { getToken, removeToken } from "../../utils/auth";
 import { useNavigate } from "react-router-dom";
 
 const LogoutButton = () => {
-    const navigate = useNavigate();
-    const handleLogout = async () => {
-        const token = getToken();
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    const token = getToken();
 
-        try {
-            const res = await fetch("http://localhost:5000/logout", {
-            method: "POST",
-            headers: {
-            "Authorization": `Bearer ${token}`,
-            "Content-Type": "application/json",
-            },
-            body: JSON.stringify({}),
-        });
+    try {
+      const res = await fetch("http://localhost:5000/auth/logout", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({}),
+      });
 
-        if (res.ok) {
-            removeToken(token);
-            navigate("/");
-        }
-        } catch (error) {
-            console.log("Error Logging out:", error);
-        }
-    };
+      if (res.ok) {
+        removeToken(token);
+        navigate("/");
+      }
+    } catch (error) {
+      console.log("Error Logging out:", error);
+    }
+  };
 
-    return (
-        <>
-        <div className="button-container">
-            <button type="button" onClick={handleLogout}>Logout</button>
-        </div>
-        </>
-    )
+  return (
+    <>
+      <div className="button-container">
+        <button type="button" onClick={handleLogout}>
+          Logout
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default LogoutButton;
