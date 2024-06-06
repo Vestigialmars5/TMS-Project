2d46cf23e19e3980f857b0798d2d88f2e0689ba5
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-06-05 22:30:46 -0400

    Add PrivateRoute

--- tms-app/src/App.jsx
+++ tms-app/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import NoPage from "./pages/no-page/NoPage";
+import AdminDashboard from ".pages/dashboard/AdminDashboard";
+import PrivateRoute from "./components/private-route/PrivateRoute";
 import "./App.css";
 import { useEffect, useState } from "react";
 
@@ -28,6 +30,7 @@ function App() {
             element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />}
           />
           <Route path="*" element={<NoPage />} />
+          <PrivateRoute path="/admin" authToken={authToken} role="admin" component={AdminDashboard} />
         </Routes>
       </BrowserRouter>
     </div>
