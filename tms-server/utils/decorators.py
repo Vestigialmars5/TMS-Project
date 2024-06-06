2f055bceed797772ba4454480d5e979c45344a38
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-06-06 17:05:59 -0400

    Remove login_required and upgrade roles_required

--- tms-server/utils/decorators.py
+++ tms-server/utils/decorators.py
@@ -1,21 +1,20 @@
-from flask import redirect, session
+from flask import jsonify, redirect, session
 from functools import wraps
 
+from flask_jwt_extended import get_jwt_identity, jwt_required
 
-# Define login_required decorator, redirects to login if not logged in
-def login_required(role_required):
+# Decorator for roles required
+def roles_required(*roles):
     def decorator(f):
         @wraps(f)
         def decorated_function(*args, **kwargs):
-            if session.get("user_id"):
-                # Check if user's role matches role required
-                if check_user_role(session.get("user_id"), role_required): # TODO: Make this function
-                    return f(*args, **kwargs)
-                else:
-                    # TODO: Make this redirect
-                    return redirect("/unauthorized")
-            else:
-                # TODO: Make this redirect
-                return redirect("/login")
+            try:
+                jwt_required()
+                current_user_role = get_jwt_identity().get("role")
+                if current_user_role not in roles:
+                    return jsonify({"error": "Unauthorized access"}), 403
+                return f(*args, **kwargs)
+            except:
+                return jsonify({"error": "Missing or invalid JWT"}), 401
         return decorated_function
     return decorator
