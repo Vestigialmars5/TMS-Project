9f35133df10d3a2c43ba194cfdbe4bb24734e8fd
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-06-05 23:39:59 -0400

    Add simple register to admin

--- tms-server/routing/admin.py
+++ tms-server/routing/admin.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, jsonify, render_template, request, url_for
+from db import get_db
+from werkzeug.security import generate_password_hash
 
 admin_blueprint = Blueprint("admin", __name__, url_prefix="/admin")
 
@@ -6,6 +8,30 @@ admin_blueprint = Blueprint("admin", __name__, url_prefix="/admin")
 @admin_blueprint.route("/")
 def index():
     return "<p>Hello, World!</p>"
+
+
+# TODO: Complete this
+@admin_blueprint.route("/register", methods=("GET", "POST"))
+def register():
+    if request.method == "POST":
+
+        email = "asdf@asdf.com"
+        username = email.split("@")[0]
+        password = "asdfasdf"
+        role = "admin"
+        
+        db = get_db()
+        try:
+            db.execute(
+                "INSERT INTO users (username, email, password, role) VALUES (?, ?, ?, ?)",
+                (username, email, generate_password_hash(password), role),
+            )
+            db.commit()
+        except Exception as e:
+            return jsonify({"success": False, "error": str(e)}), 400
+        
+    return jsonify({"success": True}), 200
+
 
 # ADMIN (permision to everything, managing accounts, system config, performance)
 # EDIT DATABSE
