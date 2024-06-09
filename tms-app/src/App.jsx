27ab734e4764939322c9f4250204d3ee2079e3e8
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-06-08 22:37:05 -0400

    Add AuthProvider

--- tms-app/src/App.jsx
+++ tms-app/src/App.jsx
@@ -3,47 +3,34 @@ import Layout from "./components/layout/Layout";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import NoPage from "./pages/no-page/NoPage";
-import AdminDashboard from "./pages/dashboard/admin/AdminDashboard";
+import AdminDashboard from "./pages/admin-pages/AdminDashboard";
 import PrivateRoute from "./components/private-route/PrivateRoute";
 import "./App.css";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [email, setEmail] = useState("");
-
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Navigate to="/home" />} />
-            <Route
-              path="/home"
-              element={
-                <Home
-                  email={email}
-                  loggedIn={loggedIn}
-                  setLoggedIn={setLoggedIn}
-                />
-              }
-            />
-            <Route
-              path="/login"
-              element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />}
-            />
-            {/* Private routes for admin */}
-            <Route
-              path="/admin"
-              element={<PrivateRoute requiredRole="admin" />}
-            >
-              <Route index element={<Navigate to="admin-dashboard" />} />
-              <Route path="admin-dashboard" element={<AdminDashboard />} />
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Navigate to="/home" />} />
+              <Route path="/home" element={<Home />} />
+
+              <Route path="/login" element={<Login />} />
+              {/* Private routes for admin */}
+              <Route
+                path="/admin"
+                element={<PrivateRoute requiredRole="admin" />}
+              >
+                <Route index element={<AdminDashboard />} />
+              </Route>
+              <Route path="*" element={<NoPage />} />
             </Route>
-            <Route path="*" element={<NoPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
