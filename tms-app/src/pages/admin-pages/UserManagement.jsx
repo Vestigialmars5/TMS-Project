55f802d803f54413814151250814ce721128bf68
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-06-10 21:57:00 -0400

    Add stuff to component

--- tms-app/src/pages/admin-pages/UserManagement.jsx
+++ tms-app/src/pages/admin-pages/UserManagement.jsx
@@ -1,1 +1,13 @@
 // Add, edit, and remove users. Assign roles and permissions.
+import React from "react";
+import AddNewUser from "../../components/admin/user-management/AddNewUser";
+
+const UserManagement = () => {
+  return (
+    <>
+      <AddNewUser />
+    </>
+  );
+};
+
+export default UserManagement;
