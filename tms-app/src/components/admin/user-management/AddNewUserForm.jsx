2f9e989a71d2a7f8e4fc81b71d9516742bc1a512
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-06-10 21:56:23 -0400

    Create AddNewUserForm

---
+++ tms-app/src/components/admin/user-management/AddNewUserForm.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AddNewUserForm = () => {
+
+    return (
+        <>
+            <h1>Add new user form</h1>
+        </>
+    )
+
+};
+
+
+
+export default AddNewUserForm;
