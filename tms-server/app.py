7ca213322ed4d4cff866e6fd3ac7b3bfa3b93298
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-06-12 20:03:44 -0400

    Add config for JWT and update imports

--- tms-server/app.py
+++ tms-server/app.py
@@ -1,14 +1,15 @@
 from flask import Flask, g
 from flask_cors import CORS
 import secrets
-from routing.admin import admin_blueprint
-from routing.auth import auth_blueprint
+from api.admin import admin_blueprint
+from api.auth import auth_blueprint
 from flask_jwt_extended import JWTManager
 
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(32)
-
+app.config["JWT_SECRET_KEY"] = secrets.token_hex(32)
+app.config["JWT_ALGORITHM"] = "HS256"
 jwt = JWTManager(app)
 
 CORS(app)
