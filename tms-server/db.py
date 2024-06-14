1135e2a386d92d15e33369eb60a92a7d265cfa64
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-06-13 23:19:16 -0400

    Add optional parameter for row factory

--- tms-server/db.py
+++ tms-server/db.py
@@ -3,11 +3,12 @@ from flask import g
 
 
 # Establish db connection, returns db connection from g
-def get_db():
+def get_db(row=True):
     # Create a connection if doesn't exist
     if "db" not in g:
         g.db = sql.connect("tms_database.db")
 
-        # Set queries to return Row objects
-        g.db.row_factory = sql.Row
+        if row:
+            # Set queries to return Row objects
+            g.db.row_factory = sql.Row
     return g.db
