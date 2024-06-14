04a6f342f7d26a4f80a460193fd357cfe6b0969c
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-06-13 23:17:38 -0400

    Add getUsers
    Also added loading and users context variables

--- tms-app/src/context/UserManagementProvider.jsx
+++ tms-app/src/context/UserManagementProvider.jsx
@@ -1,23 +1,52 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import { createUser as createUserApi } from "../utils/userManagement";
+import { getUsers as getUsersApi } from "../utils/userManagement";
 
 const UserManagementContext = createContext();
 
 export const useUserManagement = () => useContext(UserManagementContext);
 
 export const UserManagementProvider = ({ children }) => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const delayTimeout = setTimeout(() => {
+      setLoading(false);
+    }, 500); // Modify timeout for smoothness
+
+    return () => clearTimeout(delayTimeout);
+  }, []);
+
+  useEffect(() => {
+    const delayTimeout = setTimeout(() => {
+      setLoading(false);
+    }, 0); // Modify timeout for smoothness
+
+    return () => clearTimeout(delayTimeout);
+  }, []);
+
   const createUser = async (userData) => {
     try {
-        await createUserApi(userData);
+      await createUserApi(userData);
     } catch (error) {
-        console.error("Creation failed", error.message);
+      console.error("Creation failed", error.message);
+    }
+  };
+
+  const getUsers = async (args) => {
+    try {
+      const data = await getUsersApi(args);
+      setUsers(data);
+      return data;
+    } catch (error) {
+      console.error("Error with the api", error.message);
+      return {};
     }
   };
 
   return (
-    <UserManagementContext.Provider
-      value={{ createUser }}
-    >
+    <UserManagementContext.Provider value={{ createUser, getUsers, users, loading }}>
       {children}
     </UserManagementContext.Provider>
   );
