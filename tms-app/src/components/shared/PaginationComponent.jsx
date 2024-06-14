27031addcb00fc5ef9c1cad41fcb36f9f28aec5f
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-06-13 23:00:16 -0400

    Create PaginationComponent

---
+++ tms-app/src/components/shared/PaginationComponent.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Pagination from "react-bootstrap/Pagination";
+
+const PaginationComponent = ({ total, perPage, page, handlePageChange }) => {
+    const totalPages = Math.ceil(total / perPage);
+
+    return (
+        <Pagination className="mt-4">
+            {Array.from({ length: totalPages }, (_, index) => (
+                <Pagination.Item
+                    key={index + 1}
+                    active={page === index + 1}
+                    onClick={() => handlePageChange(index + 1)}
+                >
+                    {index + 1}
+                </Pagination.Item>
+            ))}
+        </Pagination>
+    );
+};
+
+export default PaginationComponent;
