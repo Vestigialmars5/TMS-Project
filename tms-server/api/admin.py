1ac96003f36b6d33cac3a4e01ded51e416f0e33c
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-06-15 16:24:34 -0400

    Move create_user to user_service

--- tms-server/api/admin.py
+++ tms-server/api/admin.py
@@ -20,41 +20,23 @@ def create_user():
         data = request.get_json()
 
         email = data.get("email")
-        username = email.split("@")[0]  # TODO: Make this different for uniqueness
         password = data.get("password")
         role = data.get("role")
 
-        try:
-            # TODO: Validations for registering
-            if not email or not username or not password or not role:
-                raise Exception("something missing")
+        response, status = UserService.create_user(email, password, role)
 
-            db = get_db()
-            db.execute(
-                "INSERT INTO users (username, email, password, role) VALUES (?, ?, ?, ?)",
-                (username, email, generate_password_hash(password), role),
-            )
-            db.commit()
-        except Exception as e:
-            return jsonify({"success": False, "error": str(e)}), 400
-
-    return jsonify({"success": True}), 200
+        return response, status
 
 
 @admin_blueprint.route("/users", methods=("GET",))
 def get_users():
     if request.method == "GET":
-        try:
-            search = request.args.get("search", "")
-            sort = request.args.get("sort", "asc")
-            page = request.args.get("page", 1, type=int)
-            limit = request.args.get("limit", 25, type=int)
 
-        except:
-            print("Error handlining request parameters")
-            return jsonify({"success": False, "error": "error setting variables"}), 200
+        search = request.args.get("search", "")
+        sort = request.args.get("sort", "asc")
+        page = request.args.get("page", 1, type=int)
+        limit = request.args.get("limit", 25, type=int)
 
         response, status = UserService.get_users(search, sort, page, limit)
 
         return jsonify(response), status
-        
