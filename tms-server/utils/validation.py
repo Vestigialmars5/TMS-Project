402c829bec90cb814dcc795f1cd05b65afde06da
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-06-15 16:23:24 -0400

    Separate validation into a separate file

--- tms-server/utils/validation.py
+++ tms-server/utils/validation.py
@@ -1,5 +1,14 @@
+# Use validator class to validate data
+from validator import Validator
+
 # TODO: Complete this
 def validate_login_credentials(email, password):
     # TODO: Validations between data being passed and from db
     ############
+    return True
+
+
+# TODO: Validations for registering
+def validate_register_credentials(email, username, password, role):
+    # TODO: Validations between data being passed and from db
     return True
