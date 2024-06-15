9226ffdfe04df58fd4f42defab29fa7c7b4d53be
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-06-15 16:22:30 -0400

    Create validator class

---
+++ tms-server/utils/validator.py
@@ -0,0 +1,38 @@
+class Validator:
+    MIN_NAME = 2
+    MAX_NAME = 30
+    MIN_PASSWORD = 8
+
+    @staticmethod
+    def is_valid_email(email):
+        # TODO: implementation here
+        pass
+
+    @staticmethod
+    def is_valid_username(username):
+        # TODO: implementation here
+        pass
+
+    @staticmethod
+    def is_valid_password(password):
+        # TODO: implementation here
+        pass
+
+    @staticmethod
+    def is_valid_role(role):
+        # TODO: implementation here
+        pass
+
+    @staticmethod
+    def is_one_word(string):
+        # TODO: implementation here
+        pass
+
+    @staticmethod
+    def has_numbers_or_specials(string):
+        # TODO: implementation here
+        pass
+
+    @staticmethod
+    def is_valid_length(string):
+        return Validator.MIN_NAME <= len(string) <= Validator.MAX_NAME
