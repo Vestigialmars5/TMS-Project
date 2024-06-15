2f132029fc896f948295d728043b25b484320427
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-06-15 19:02:26 -0400

    Set up a base layout

--- tms-app/src/components/shared/UserCard.jsx
+++ tms-app/src/components/shared/UserCard.jsx
@@ -4,17 +4,28 @@ import Col from "react-bootstrap/Col";
 
 const UserCard = ({ user }) => {
   return (
-    <Col sm={12} md={6} lg={4} className="mb-4">
-      <Card>
-        <Card.Body>
-          <Card.Title>{user.username}</Card.Title>
-          <Card.Text>{user.id}</Card.Text>
-          <Card.Text>{user.email}</Card.Text>
-          <Card.Text>{user.role}</Card.Text>
-        </Card.Body>
-      </Card>
-    </Col>
-    
+    <>
+      {user ? (
+        <ListGroup horizontal>
+          <ListGroup.Item>{user.username}</ListGroup.Item>
+          <ListGroup.Item>{user.id}</ListGroup.Item>
+          <ListGroup.Item>{user.email}</ListGroup.Item>
+          <ListGroup.Item>{user.role}</ListGroup.Item>
+          <ListGroup.Item>
+            <button onClick={handleUserEdit}>Edit</button>
+            <button onClick={handleUserDelete}>Delete</button>
+          </ListGroup.Item>
+        </ListGroup>
+      ) : (
+        <ListGroup horizontal>
+          <ListGroup.Item>Username</ListGroup.Item>
+          <ListGroup.Item>Id</ListGroup.Item>
+          <ListGroup.Item>Email</ListGroup.Item>
+          <ListGroup.Item>Role</ListGroup.Item>
+          <ListGroup.Item>Actions</ListGroup.Item>
+        </ListGroup>
+      )}
+    </>
   );
 };
 
