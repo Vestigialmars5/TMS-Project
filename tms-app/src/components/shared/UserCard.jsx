601127851e97e63242936090aa43c7f6d1b2632c
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-06-16 18:52:23 -0400

    chore: Add user edit functionality and confirm dialog for user deletion

--- tms-app/src/components/shared/UserCard.jsx
+++ tms-app/src/components/shared/UserCard.jsx
@@ -1,45 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import ListGroup from "react-bootstrap/ListGroup";
+import EditUser from "../admin/user-management/EditUser";
 import { useUserManagement } from "../../context/UserManagementProvider";
 
 const UserCard = ({ user }) => {
-  const { deleteUser } = useUserManagement();
+  const { deleteUser, refreshUsers } = useUserManagement();
+  const [isEditing, setIsEditing] = useState(false);
 
-  const handleUserDelete = async () => {
+  const handleDeleteUser = async () => {
     console.log("Delete user by id", user.id);
+    // TODO: Make it better Confirm dialog
+    const confirmDelete = window.confirm(
+      "Are you sure you want to delete this user?"
+    );
+    if (confirmDelete) {
     try {
       await deleteUser(user.id);
+        await refreshUsers();
     } catch (error) {
       console.error(error.message);
+      }
     }
   };
 
-  const handleUserEdit = () => {
-    console.log("Edit user");
+  const handleEdit = () => {
+    console.log("Edit user by id", user.id);
+    setIsEditing(true);
+  };
+
+  const cancelEdit = () => {
+    console.log("Canceled edit");
+    setIsEditing(false);
   };
 
   return (
     <>
-      {user ? (
+      {isEditing && <EditUser user={user} cancelEdit={cancelEdit} />}
         <ListGroup horizontal>
           <ListGroup.Item>{user.username}</ListGroup.Item>
           <ListGroup.Item>{user.id}</ListGroup.Item>
           <ListGroup.Item>{user.email}</ListGroup.Item>
           <ListGroup.Item>{user.role}</ListGroup.Item>
           <ListGroup.Item>
-            <button onClick={handleUserEdit}>Edit</button>
-            <button onClick={handleUserDelete}>Delete</button>
+          <button onClick={handleEdit}>Edit</button>
+          <button onClick={handleDeleteUser}>Delete</button>
           </ListGroup.Item>
         </ListGroup>
-      ) : (
-        <ListGroup horizontal>
-          <ListGroup.Item>Username</ListGroup.Item>
-          <ListGroup.Item>Id</ListGroup.Item>
-          <ListGroup.Item>Email</ListGroup.Item>
-          <ListGroup.Item>Role</ListGroup.Item>
-          <ListGroup.Item>Actions</ListGroup.Item>
-        </ListGroup>
-      )}
     </>
   );
 };
