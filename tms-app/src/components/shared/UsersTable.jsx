1e343e85024271566287f8ab1c64923932987c79
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-06-16 18:53:05 -0400

    chore: Add UsersTable component for displaying users in a table format

---
+++ tms-app/src/components/shared/UsersTable.jsx
@@ -0,0 +1,30 @@
+import { useUserManagement } from "../../context/UserManagementProvider";
+import ListGroup from "react-bootstrap/ListGroup";
+import Spinner from "react-bootstrap/Spinner";
+import UserCard from "../../components/shared/UserCard";
+import { useEffect } from "react";
+
+const UsersTable = () => {
+  const { loading, users } = useUserManagement();
+
+  useEffect(() => {}, [users]);
+
+  return (
+    <>
+      <ListGroup horizontal>
+        <ListGroup.Item>Username</ListGroup.Item>
+        <ListGroup.Item>Id</ListGroup.Item>
+        <ListGroup.Item>Email</ListGroup.Item>
+        <ListGroup.Item>Role</ListGroup.Item>
+        <ListGroup.Item>Actions</ListGroup.Item>
+      </ListGroup>
+      {loading ? (
+        <Spinner animation="border" />
+      ) : (
+        users.map((user, index) => <UserCard key={index} user={user} />)
+      )}
+    </>
+  );
+};
+
+export default UsersTable;
