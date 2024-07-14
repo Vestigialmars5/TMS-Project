bbf2d47b742ff1a068f18d2540032edb8a512972
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-07-14 15:33:16 -0400

    Add: Add optional parameter for db
    - get_db now accepts an optional parameter to specify the database to use
    - tms-database is the default value

--- tms-server/db.py
+++ tms-server/db.py
@@ -3,10 +3,10 @@ from flask import g
 
 
 # Establish db connection, returns db connection from g
-def get_db(row=True):
+def get_db(row=True, db_name="tms_database.db"):
     # Create a connection if doesn't exist
     if "db" not in g:
-        g.db = sql.connect("tms_database.db")
+        g.db = sql.connect(db_name)
 
         if row:
             # Set queries to return Row objects
