013b7f3fe0db3f1867a58adc7b52498457b673d9
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-07-21 12:18:21 -0400

    Add, Fix: Implement the factory pattern for the Flask app
    - Uses instance folder for storing database
    - Uses environment variables for configuration
    - Uses a configuration file for the app
    - Deleted unnecessary files
      - jwt_config.py
    - Logs is now stored in the instance folder
    - Database is now stored in the instance folder
    - Updated .gitignore to ignore instance folder

--- server/config.py
+++ server/config.py
@@ -1,9 +1,17 @@
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 class Config:
     """Base configuration"""
-    SECRET_KEY = os.environ.get('SECRET KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    JWT_ALGORITHM = os.getenv('JWT_ALGORITHM')
+    JWT_ACCESS_TOKEN_EXPIRES = int(os.getenv('JWT_ACCESS_TOKEN_EXPIRES'))
+    JWT_REFRESH_TOKEN_EXPIRES = int(os.getenv('JWT_REFRESH_TOKEN_EXPIRES'))
+    DATABASE_URL = os.getenv('DATABASE_URL')
+    WMS_DATABASE_URL = os.getenv('WMS_DATABASE_URL')
 
 
 class Development(Config):
