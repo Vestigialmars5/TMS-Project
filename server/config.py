3b9c5ba18f3f35ba1d3b26358c7c483b6631bff8
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-07-24 23:13:18 -0400

    Wip: Update database URLs and SQLAlchemy configurations
    - Update `DATABASE_URL` to `TMS_DATABASE_URL` in the `Config` class
    - Add `SQLALCHEMY_DATABASE_URI` and `SQLALCHEMY_BINDS` configurations for different environments

--- server/config.py
+++ server/config.py
@@ -10,20 +10,35 @@ class Config:
     JWT_ALGORITHM = os.getenv('JWT_ALGORITHM')
     JWT_ACCESS_TOKEN_EXPIRES = int(os.getenv('JWT_ACCESS_TOKEN_EXPIRES'))
     JWT_REFRESH_TOKEN_EXPIRES = int(os.getenv('JWT_REFRESH_TOKEN_EXPIRES'))
-    DATABASE_URL = os.getenv('DATABASE_URL')
+    TMS_DATABASE_URL = os.getenv('DATABASE_URL')
     WMS_DATABASE_URL = os.getenv('WMS_DATABASE_URL')
 
 
 class Development(Config):
     """Development configuration"""
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = Config.TMS_DATABASE_URL
+    SQLACLHEMY_BINDS = {
+        "wms": Config.WMS_DATABASE_URL
+    }
 
 
 class Testing(Config):
     """Testing configuration"""
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+    SQLACLHEMY_BINDS = {
+        "wms": "sqlite:///:memory:"
+    }
+    
 
 
 class Production(Config):
     """Production configuration"""
     DEBUG = False
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = Config.TMS_DATABASE_URL
+    SQLACLHEMY_BINDS = {
+        "wms": Config.WMS_DATABASE_URL
+    }
+
