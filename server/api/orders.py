1a4c4a4e7b4a8f65a97fd85bc7c4e0bedbbed446
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-08-01 13:21:59 -0400

    Wip, Refactor: Refactor to sqlalchemy
    - Wip, because I need to add an additional table for company
    and that is for another branch. So this file is incomplete and unusable
    right now.

--- server/api/orders.py
+++ server/api/orders.py
@@ -1,7 +1,8 @@
-# Receive and process orders from the WMS 
+# Receive and process orders from the WMS
 
 from flask import request, Blueprint
-from server.db import get_db
+from server.extensions import db
+from server.models.tms_models import Order, OrderDetails
 
 orders_blueprint = Blueprint("orders", __name__, url_prefix="/orders")
 
@@ -12,41 +13,18 @@ def receive_order():
     if request.method == "POST":
         data = request.get_json()
 
-        order_uuid = data.get("order_id") # When communicating with the wms, refer to the uuid
+        # When communicating with the wms, refer to the uuid
+        order_uuid = data.get("order_id")
         warehouse_id = data.get("warehouse_id")
         products = data.get("products")
         total_weight = data.get("total_weight")
         total_volume = data.get("total_volume")
 
         # Save the order to the database
-        save_order(order_uuid, warehouse_id, products, total_weight, total_volume)
+        save_order(order_uuid, warehouse_id, products,
+                   total_weight, total_volume)
 
 
 # Save the order to the database
 def save_order(order_uuid, warehouse_id, products, total_weight, total_volume):
-    db = get_db()
-    cursor = db.cursor()
-
-    try:
-        cursor.execute(
-            """INSERT INTO orders (order_uuid, warehouse_id, total_weight, total_volume) VALUES (?, ?, ?, ?)""", (order_uuid, warehouse_id, total_weight, total_volume)
-        )
-
-        order_id = cursor.lastrowid
-
-        for product in products:
-            cursor.execute(
-                """INSERT INTO order_products (order_id, product_id, product_name, supplier_id, priority, quantity, weight, volume) VALUES (?, ?, ?, ?, ?, ?, ?, ?)""",
-                (order_id, product["product_id"], product["product_name"], product["supplier_id"], product["priority"], product["quantity"], product["weight"], product["volume"])
-            )
-        
-        db.commit()
-    except Exception as e:
-        db.rollback()
-        print(e)
-    
-    finally:
-        cursor.close()
-    
-
-
+    order = Order(order_uuid=order_uuid, customer_id=)
