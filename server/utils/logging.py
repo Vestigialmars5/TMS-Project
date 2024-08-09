acd3c4bd1c0de74b763a088fa5791db5d4b11883
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-08-09 16:42:24 -0400

    Add: Implement optional id or email for AuditLog

--- server/utils/logging.py
+++ server/utils/logging.py
@@ -1,12 +1,8 @@
 from server.extensions import db
 from server.models.tms_models import AuditLog
 
-def log_error(e):
-    pass
-
-def create_audit_log(user_id, action, details):
-    audit_log = AuditLog(
-        user_id=user_id, action=action, details=details
-    )
+def create_audit_log(action, user_id=None, email=None, details=None):
+    audit_log = AuditLog(action=action, user_id=user_id,
+                         email=email, details=details)
     db.session.add(audit_log)
     db.session.commit()
