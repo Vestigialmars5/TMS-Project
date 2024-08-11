8451cab7832bf6037e82abd6b94caa3554d78985
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-08-11 00:02:59 -0400

    Update: Update deprecated

--- server/db_initiator.py
+++ server/db_initiator.py
@@ -7,11 +7,8 @@ import click
 
 def init_db():
     with current_app.app_context():
-        print("Creating dbs")
-        print(f"tms db is being created at {db.get_engine(current_app, bind=None)}")
-        print(f"wms db is being created at {db.get_engine(current_app, bind='wms')}")
-        Base1.metadata.create_all(bind=db.get_engine(current_app, bind=None))
-        Base2.metadata.create_all(bind=db.get_engine(current_app, bind='wms'))
+        Base1.metadata.create_all(bind=db.engine)
+        Base2.metadata.create_all(bind=db.engines["wms"])
 
 @click.command("init-db")
 def init_db_command():
--- server/models/base.py
+++ server/models/base.py
@@ -1,4 +1,4 @@
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 Base1 = declarative_base()
 Base2 = declarative_base()
