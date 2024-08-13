65e97df65cb6b1be7fcb6616e3d24118b75fb130
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-08-12 23:13:09 -0400

    Add, wip: Create validations for login, and common ones
    - Probably need to change validator.py to another name, and maybe subdivide in different folders

--- server/utils/validation.py
+++ server/utils/validation.py
@@ -1,13 +1,33 @@
 # Use validator class to validate data
-from server.utils.validator import Validator
+from server.utils.validator import *
+from server.extensions import db
+from server.models.tms_models import *
+from werkzeug.security import check_password_hash
+from server.services.exceptions import DatabaseQueryError
 
 # TODO: Get roles from db
 
 # TODO: Complete this
 def validate_login_credentials(email, password):
-    # TODO: Validations between data being passed and from db
-    ############
+    email = clean_email(email)
+
+    user = db.session.query(User).filter(User.email == email).first()
+    if not user:
+        return False
+    
+    # TODO: Uncomment -Ignores if password is wrong
+    """ if not check_password_hash(user.password, password):
+        return False """
+
     return True
+
+def get_first_name_last_name(user_id):
+    query = db.select(UserDetails.first_name, UserDetails.last_name).filter(
+        UserDetails.user_id == user_id)
+    res = db.session.execute(query).first()
+    if not res:
+        return None, None
+    return res.first_name, res.last_name
 
 
 # TODO: Validations for registering
--- server/utils/validator.py
+++ server/utils/validator.py
@@ -1,38 +1,24 @@
-class Validator:
-    MIN_NAME = 2
-    MAX_NAME = 30
-    MIN_PASSWORD = 8
+import re
 
-    @staticmethod
-    def is_valid_email(email):
-        # TODO: implementation here
-        pass
+MIN_NAME = 2
+MAX_NAME = 30
+MIN_PASSWORD = 8
 
-    @staticmethod
-    def is_valid_username(username):
-        # TODO: implementation here
-        pass
+def clean_string(s):
+    return s.strip()
 
-    @staticmethod
-    def is_valid_password(password):
-        # TODO: implementation here
-        pass
+def clean_email(email):
+    return email.strip().lower()
 
-    @staticmethod
-    def is_valid_role(role):
-        # TODO: implementation here, return role id
-        pass
+def clean_name(name):
+    return name.strip().title()
 
-    @staticmethod
-    def is_one_word(string):
-        # TODO: implementation here
-        pass
+def is_valid_email(email):
+    regex = r'[^@]+@[^@]+\.[^@]+'
+    return re.match(regex, email)
 
-    @staticmethod
-    def has_numbers_or_specials(string):
-        # TODO: implementation here
-        pass
+def is_strong_password(password):
+    return len(password) >= MIN_PASSWORD
 
-    @staticmethod
-    def is_valid_length(string):
-        return Validator.MIN_NAME <= len(string) <= Validator.MAX_NAME
+def is_valid_name(name):
+    return MIN_NAME <= len(name) <= MAX_NAME
