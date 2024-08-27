e1ff2b85972df8b87a01c130ce9d81507a852d81
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-08-27 10:42:42 -0600

    Update, Add: User exists funcitons
    - Added a get_user for cases where I want to keep using that found user
    - Moved the return statements to avoid defining user every line and implicitly return bool if None

--- server/utils/validations.py
+++ server/utils/validations.py
@@ -4,26 +4,38 @@ from server.extensions import db
 from server.models.tms_models import *
 from werkzeug.security import check_password_hash
 from server.utils.exceptions import DatabaseQueryError
+from server.utils.helpers import create_unique_username
 
 
 def user_exists(user_id=None, email=None, username=None):
     try:
         if user_id:
-            user = db.session.query(User).filter(User.user_id == user_id).first()
+            return db.session.query(User).filter(User.user_id == user_id).first() is not None
         elif email:
-            user = db.session.query(User).filter(User.email == email).first()
+            return db.session.query(User).filter(User.email == email).first() is not None
         elif username:
-            user = db.session.query(User).filter(User.username == username).first()
+            return db.session.query(User).filter(User.username == username).first() is not None
         else:
             return False
     except Exception as e:
         raise DatabaseQueryError("Error Finding User")
 
-    if not user:
-        return False
 
-    return True
-
+# Different from user_exists. I rather have two different functions
+def get_user(user_id=None, email=None, username=None):
+    try:
+        if user_id:
+            return db.session.query(User).filter(
+                User.user_id == user_id).first()
+        elif email:
+            return db.session.query(User).filter(User.email == email).first()
+        elif username:
+            return db.session.query(User).filter(
+                User.username == username).first()
+        else:
+            return None
+    except Exception as e:
+        raise DatabaseQueryError("Error Getting User")
 
 def is_password_valid(password):
     if not password or not isinstance(password, str):
@@ -48,5 +60,21 @@ def validate_delete_user(user_id, initiator_id):
 
     if not user_exists(user_id=user_id):
         return False, "User Does Not Exist"
+
+    return True
+
+
+def validate_update_user(user_id, username, email, role_id):
+    user = get_user(user_id=user_id)
+
+    if not user:
+        return False, "User Does Not Exist"
+
+    if user.username == username and user.email == email and user.role_id == role_id:
+        return False, "No Changes Made"
+
+    if user_exists(username=username):
+        recommended = create_unique_username(username)
+        return False, "Username Already Exists: Recommended {recommended}"
 
     return True
