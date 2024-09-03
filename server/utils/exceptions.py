3f2d204e7d76ef9765c55e8cc25fe9ea7d2cb711
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-09-02 23:13:38 -0600

    Update: status code 403 -> 400 for data validation error

--- server/utils/exceptions.py
+++ server/utils/exceptions.py
@@ -15,7 +15,7 @@ class ValidationError(Exception):
 
 
 class DataValidationError(ValidationError):
-    status_code = 403
+    status_code = 400
     def __init__(self, message="Invalid Data"):
         self.message = message
         super().__init__(self.message)
