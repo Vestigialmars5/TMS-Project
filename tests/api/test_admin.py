4f7a31efc4578b02af2bc344b31136f69fe02f9b
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-09-06 16:22:57 -0400

    Fix, Refactor: tests get_user and delete_user
    - Fixed get_users. Was using carrier_token instead of "carrier_token"
    - Removed return statement from all tests
    - Refactored delete_user to use parametrize
    - Added test cases for delete_user
        - Using "self" or "complete_user_id" instead of hardcoding the IDs
        - Added if statements to check what id to get

--- tests/api/test_admin.py
+++ tests/api/test_admin.py
@@ -47,8 +47,6 @@ def test_create_user(client, token_fixture, email, password, role_id, expected_s
     assert response.status_code == expected_status_code
     assert response.json["success"] == expected_success
 
-    return response
-
 
 get_users_test_cases = [
     # Test case 1: Get users with valid inputs
@@ -56,7 +54,7 @@ get_users_test_cases = [
     # Test case 2: Get users with no token
     ("", "admin", "email", "asc", 1, 2, 401, False),
     # Test case 3: Get users with invalid token
-    (carrier_token, "admin", "email", "asc", 1, 2, 401, False),
+    ("carrier_token", "admin", "email", "asc", 1, 2, 401, False),
     # Test case 4: Get users with invalid search
     ("admin_token", 1, "email", "asc", 1, 2, 200, True),
     # Test case 5: Get users with invalid sort_by
@@ -84,106 +82,82 @@ def test_get_users(client, token_fixture, search_field, sort_by, sort_order, pag
     assert response.status_code == expected_status_code
     assert response.json["success"] == expected_success
 
-    return response
+
+delete_user_test_cases = [
+    # Test case 1: Delete user with valid user_id
+    ("admin_token", "complete_user_id", 200, True),
+
+    # Test case 2: Delete user with non-existing user_id
+    ("admin_token", 1000, 404, False),
+
+    # Test case 3: Delete user with no token
+    ("", "complete_user_id", 401, False),
+
+    # Test case 4: Delete user with invalid token
+    ("carrier_token", "complete_user_id", 401, False),
+
+    # Test case 5: Delete self
+    ("admin_token", "self", 403, False)
+]
 
 
-def test_get_users(client, admin_token, carrier_token):
-    # Test case 1: Get users with valid inputs
-    get_users(client, admin_token, "admin", "username", "asc", 1, 2, 200, True)
+@pytest.mark.parametrize("token_fixture, user_id, expected_status_code, expected_success", delete_user_test_cases, ids=["1", "2", "3", "4", "5"], indirect=["token_fixture"])
+def test_delete_user(client, token_fixture, user_id, expected_status_code, expected_success):
+    if user_id == "self":
+        user_id = db.session.query(User).filter_by(
+            email=consts.ADMIN_EMAIL).first().user_id
+    elif user_id == "complete_user_id":
+        user_id = db.session.query(User).filter_by(
+            email=consts.COMPLETE_USER_EMAIL).first().user_id
 
-    # Test case 2: Get users with no token
-    get_users(client, "", "admin", "username", "asc", 1, 2, 401, False)
-
-    # Test case 3: Get users with invalid token
-    get_users(client, carrier_token, "admin", "username", "asc", 1, 2, 401, False)
-
-    # Test case 4: Get users with invalid search
-    get_users(client, admin_token, "invalid_search", "username", "asc", 1, 2, 400, False)
-
-    # Test case 5: Get users with invalid sort_by
-    get_users(client, admin_token, "admin", "invalid_sort_by", "asc", 1, 2, 400, False)
-
-    # Test case 6: Get users with invalid sort_order
-    get_users(client, admin_token, "admin", "username", "invalid_sort_order", 1, 2, 400, False)
-
-    # Test case 7: Get users with invalid page
-    get_users(client, admin_token, "admin", "username", "asc", 0, 2, 400, False)
-
-    # Test case 8: Get users with invalid limit
-    get_users(client, admin_token, "admin", "username", "asc", 1, 0, 400, False)
-
-    # Test case 9: Get users with no search, sort_by, sort_order, page, limit
-    get_users(client, admin_token, "", "", "", 1, 2, 200, True)
-
-
-def delete_user(client, token, user_id, expected_status_code, expected_success):
     response = client.delete(f"/api/admin/users/{user_id}", headers={
-        "Authorization": f"Bearer {token}",
+        "Authorization": f"Bearer {token_fixture}",
         "Content-Type": "application/json"
     }, json={})
 
     assert response.status_code == expected_status_code
     assert response.json["success"] == expected_success
 
-    return response
 
-
-def test_delete_user(client, admin_token, carrier_token):
-    # Test case 1: Delete user with valid user_id
-    delete_user(client, admin_token, 1, 200, True)
-
-    # Test case 2: Delete user with non-existing user_id
-    delete_user(client, admin_token, 1000, 400, False)
-
-    # Test case 3: Delete user with no token
-    delete_user(client, "", 1, 401, False)
-
-    # Test case 4: Delete user with invalid token
-    delete_user(client, carrier_token, 1, 401, False)
-
-    # Test case 5: Delete self
-    admin_id = db.session.query(User).filter_by(email=consts.ADMIN_EMAIL).first().user_id
-    delete_user(client, admin_token, admin_id, 403, False)
-
-
-def update_user(client, token, user_id, email, username, role_id, expected_status_code, expected_success):
+def update_user(client, token, user_id, email, role_id, expected_status_code, expected_success):
     response = client.put(f"/api/admin/users/{user_id}", headers={
         "Authorization": f"Bearer {token}",
         "Content-Type": "application/json"
     }, json={
         "email": email,
-        "username": username,
         "roleId": role_id
     })
 
     assert response.status_code == expected_status_code
     assert response.json["success"] == expected_success
 
-    return response
-
 
 def test_update_user(client, admin_token, carrier_token):
     # Test case 1: Update user with valid inputs
-    update_user(client, admin_token, 1, "updated@gmail.com", "updated2", 2, 200, True)
-    
+    update_user(client, admin_token, 1, "updated@gmail.com",
+                "updated2", 2, 200, True)
+
     # Test case 2: Update user with non-existing user_id
-    update_user(client, admin_token, 1000, "updated@gmail.com", "updated1", 1, 400, False)
+    update_user(client, admin_token, 1000,
+                "updated@gmail.com", "updated1", 1, 400, False)
 
     # Test case 3: Update user with invalid email
-    update_user(client, admin_token, 1, "invalid_email", "updated1", 1, 400, False)
+    update_user(client, admin_token, 1, consts.INVALID_EMAIL,
+                "updated1", 1, 400, False)
 
-    # Test case 4: Update user with invalid username
-    update_user(client, admin_token, 1, "updated@gmail.com", "", 1, 400, False)
+    # Test case 4: Update user with invalid role_id
+    update_user(client, admin_token, 1, "updated@gmail.com",
+                "updated1", 100, 400, False)
 
-    # Test case 5: Update user with invalid role_id
-    update_user(client, admin_token, 1, "updated@gmail.com", "updated1", 100, 400, False)
-
-    # Test case 6: Update user with no token
+    # Test case 5: Update user with no token
     update_user(client, "", 1, "updated@gmail.com", "updated1", 1, 401, False)
 
-    # Test case 7: Update user with invalid token
-    update_user(client, carrier_token, 1, "updated@gmail.com", "updated1", 1, 401, False)
+    # Test case 6: Update user with invalid token
+    update_user(client, carrier_token, 1, "updated@gmail.com",
+                "updated1", 1, 401, False)
 
-    # Test case 8: No changes made
-    complete_user_id = db.session.query(User).filter_by(email=consts.COMPLETE_USER_EMAIL).first().user_id
-    update_user(client, admin_token, complete_user_id, consts.COMPLETE_USER_EMAIL, consts.COMPLETE_USER_USERNAME, 1, 200, True)
+    # Test case 7: No changes made
+    complete_user_id = db.session.query(User).filter_by(
+        email=consts.COMPLETE_USER_EMAIL).first().user_id
+    update_user(client, admin_token, complete_user_id,
+                consts.COMPLETE_USER_EMAIL, 1, 200, True)
