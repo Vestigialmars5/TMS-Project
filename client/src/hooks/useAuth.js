b2df8004eb515a7de5944f9d3eeb3ef8680e98f0
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-09-14 22:50:08 -0400

    Add: Implement useAuth hook for user authentication

---
+++ client/src/hooks/useAuth.js
@@ -0,0 +1,32 @@
+import { useDispatch, useSelector } from "react-redux";
+import { loginUser } from "../store/slices/authSlice";
+import { useRoleBasedNavigation } from "../hooks/useRoleBasedNavigation";
+import { useNavigate } from "react-router-dom";
+
+export const useAuth = () => {
+  const dispatch = useDispatch();
+  const authState = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+  const { goToDashboard } = useRoleBasedNavigation();
+
+  const login = async (credentials) => {
+    try {
+      const user = await dispatch(loginUser(credentials)).unwrap();
+      goToDashboard(user.roleId);
+    } catch (error) {
+      // Do nothing
+      console.log();
+    }
+  };
+
+  const logout = () => {
+    dispatch(logoutUser());
+    navigate("/login");
+  };
+
+  return {
+    ...authState,
+    login,
+    logout,
+  };
+};
