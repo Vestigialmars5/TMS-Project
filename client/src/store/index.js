b7f2f96ec6be204853b098cfb3497c82b6041a33
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-09-15 21:04:17 -0400

    Add: Implement redux-persist
    - Ignore redux-persist non-serializable error
    - Added the slices to rootReducer to combine them now that I'm using persist
    - Changed reducer definition in configureStore from reducer: {reducer:...}
    to reducer: persistedReducer

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,9 +1,36 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./slices/authSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import rootReducer from "./rootReducer";
 
-export const store = configureStore({
-    reducer: {
-        auth: authReducer,
-    },
-    devTools: process.env.NODE_ENV !== "production",
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["auth"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV !== "production",
 });
+
+const persistor = persistStore(store);
+
+export { store, persistor };
