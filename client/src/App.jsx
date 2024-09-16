dbc24050efcf044879a113c443e08e0e7ed450ed
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-09-15 20:50:24 -0400

    Refactor: routing configuration and add Redux store provider
    - Move the <BrowserRouter> component from App.jsx to main.jsx to encapsulate the entire application routing.
    - Wrap the <App> component with the <Provider> component from react-redux to provide the Redux store to all components.
    - Add the <PersistGate> component from redux-persist to ensure persistence of the Redux store during page reloads.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,59 +10,51 @@ import { AuthProvider } from "./context/AuthProvider";
 import { UserManagementProvider } from "./context/UserManagementProvider";
 import { AlertProvider } from "./context/AlertProvider";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import Onboarding from "./pages/onboarding/Onboarding";
 import PrivateRoute from "./routes/PrivateRoute";
 import AdminRoute from "./routes/AdminRoute";
 import PublicRoute from "./routes/PublicRoute";
-import { Provider, useSelector } from "react-redux";
-import { store } from "./store/index";
 
 function App() {
-
   return (
-    <BrowserRouter>
-      <Provider store={store}>
-        <AuthProvider>
-          <AlertProvider>
-            <Routes>
-              <Route path="/" element={<Layout />}>
-                <Route index element={<Navigate to="/home" />} />
-                <Route path="/home" element={<Home />} />
+    <AuthProvider>
+      <AlertProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Navigate to="/home" />} />
+            <Route path="/home" element={<Home />} />
 
-                {/* Logged out only */}
-                <Route path="/login" element={<PublicRoute />}>
-                  <Route index element={<Login />} />
-                </Route>
-                {/* End logged out only */}
+            {/* Logged out only */}
+            <Route path="/login" element={<PublicRoute />}>
+              <Route index element={<Login />} />
+            </Route>
+            {/* End logged out only */}
 
-                {/* Authenticated not logged in only, any user */}
-                <Route
-                  path="/onboarding"
-                  element={<PrivateRoute required={["not_onboarded"]} />}
-                >
-                  <Route index element={<Onboarding />} />
-                </Route>
-                {/* End authenticated not logged in only, any user */}
+            {/* Authenticated not logged in only, any user */}
+            <Route
+              path="/onboarding"
+              element={<PrivateRoute required={["not_onboarded"]} />}
+            >
+              <Route index element={<Onboarding />} />
+            </Route>
+            {/* End authenticated not logged in only, any user */}
 
-                {/* Private routes for admin */}
-                <Route path="/admin" element={<AdminRoute />}>
-                  <Route index element={<AdminDashboard />} />
-                  <Route
-                    path="user-management"
-                    element={<UserManagement />}
-                    exact
-                  />
-                </Route>
-                {/* End private routes for admin */}
+            {/* Private routes for admin */}
+            <Route path="/admin" element={<AdminRoute />}>
+              <Route index element={<AdminDashboard />} />
+              <Route
+                path="user-management"
+                element={<UserManagement />}
+                exact
+              />
+            </Route>
+            {/* End private routes for admin */}
 
-                <Route path="*" element={<NoPage />} />
-              </Route>
-            </Routes>
-          </AlertProvider>
-        </AuthProvider>
-      </Provider>
-    </BrowserRouter>
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </AlertProvider>
+    </AuthProvider>
   );
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./store/index.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
