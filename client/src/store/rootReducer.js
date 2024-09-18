3f07a7e4807137611ef38fa12bb7f83bf7948c73
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-09-18 19:53:59 -0400

    Add: Create alerts slice
    - Add the alertsReducer to rootReducer
    - Add alerts whitelist
    - Create the slice with reducers for adding, removing and clearing alerts

--- client/src/store/rootReducer.js
+++ client/src/store/rootReducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
+import alertsReducer from "./slices/alertsSlice";
 
 
 export const rootReducer = combineReducers({
     auth: authReducer,
+    alerts: alertsReducer,
 });
 
 
