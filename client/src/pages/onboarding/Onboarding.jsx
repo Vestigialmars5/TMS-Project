8d5f8a8daa30f1ae9467b7cd6f65356ee8ac36c7
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-09-20 15:37:52 -0400

    Refactor: Move form to new component

--- client/src/pages/onboarding/Onboarding.jsx
+++ client/src/pages/onboarding/Onboarding.jsx
@@ -1,125 +1,13 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
-import { useAuth } from "../../context/AuthProvider";
-import { useAlertOld } from "../../context/AlertProvider";
-import { Form, Button, Spinner } from "react-bootstrap";
-import { onboardUserApi } from "../../utils/onboarding";
-import { navigateBasedOnRole } from "../../utils/navigation";
-import { useNavigate } from "react-router-dom";
+import React from "react";
+import UserDetailsForm from "../../components-new/onboarding/userDetailsForm";
 
 const Onboarding = () => {
-  const { user, updateUser } = useAuth();
-  const { addAlertOld } = useAlertOld();
-  const navigate = useNavigate();
-  const [userData, setUserData] = useState({
-    email: user.email,
-    password: "",
-    confirmation: "",
-    firstName: "",
-    lastName: "",
-    phoneNumber: "",
-    address: "",
-  });
-
-  // TODO: Validations
-
-  const handleChange = (e) => {
-    setUserData({ ...userData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    // TODO: Validations
-    try {
-      console.log(userData);
-      const updatedUserData = await onboardUserApi({ userData });
-      updateUser(updatedUserData);
-      addAlertOld("Onboarding successful", "success");
-      navigateBasedOnRole(user.roleName, navigate); // Onboarding does't modify role, safe to use from original user
-    } catch (error) {
-      addAlertOld(error.message, "danger");
-      // TODO: Logout
-    }
-  };
 
   return (
-    <>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            type="email"
-            name="email"
-            value={userData.email}
-            readOnly
-          />
-        </Form.Group>
-
-        <Form.Group controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            name="password"
-            value={userData.password}
-            onChange={handleChange}
-          />
-        </Form.Group>
-
-        <Form.Group controlId="confirmation">
-          <Form.Label>Confirm Password</Form.Label>
-          <Form.Control
-            type="password"
-            name="confirmation"
-            value={userData.confirmation}
-            onChange={handleChange}
-          />
-        </Form.Group>
-        <Form.Group controlId="firstName">
-          <Form.Label>First Name</Form.Label>
-          <Form.Control
-            type="text"
-            name="firstName"
-            value={userData.firstName}
-            onChange={handleChange}
-          />
-        </Form.Group>
-
-        <Form.Group controlId="lastName">
-          <Form.Label>Last Name</Form.Label>
-          <Form.Control
-            type="text"
-            name="lastName"
-            value={userData.lastName}
-            onChange={handleChange}
-          />
-        </Form.Group>
-
-        <Form.Group controlId="phoneNumber">
-          <Form.Label>Phone Number</Form.Label>
-          <Form.Control
-            type="tel"
-            name="phoneNumber"
-            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-            value={userData.phoneNumber}
-            onChange={handleChange}
-          />
-        </Form.Group>
-
-        <Form.Group controlId="address">
-          <Form.Label>Address</Form.Label>
-          <Form.Control
-            type="text"
-            name="address"
-            value={userData.address}
-            onChange={handleChange}
-          />
-        </Form.Group>
-
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-    </>
+    <div>
+      <h1>Onboarding</h1>
+      <UserDetailsForm />
+    </div>
   );
 };
 
