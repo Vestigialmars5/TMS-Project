792300b0e95fd071432d469a77d2522283e51f1f
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-09-23 18:06:19 -0400

    Add: react-query-devtools

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,7 +8,7 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./store/index.js";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -18,8 +18,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <QueryClientProvider client={queryClient}>
-          <App />
-          <ReactQueryDevtools />
+            <App />
+            <ReactQueryDevtools initialIsOpen />
           </QueryClientProvider>
         </PersistGate>
       </Provider>
