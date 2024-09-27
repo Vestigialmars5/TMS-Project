345122c19a2a1501e181487c3d46c36c6d6c68f7
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-09-26 22:01:16 -0400

    Add: Create new UserManagement page

---
+++ client/src/pages-new/admin/UserManagement.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const UserManagement = () => {
+  return (
+    <div>
+      <h1>User Management</h1>
+      <UsersForm />
+      <UsersList />
+    </div>
+  );
+};
+
+export default UserManagement;
