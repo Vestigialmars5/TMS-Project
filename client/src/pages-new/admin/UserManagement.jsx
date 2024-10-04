18303277a7af39422b84dced475b6a4f2058e5e4
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-10-04 00:02:58 -0400

    Wip: Uncomment UsersList
    - Delete and edit functionality still missing

--- client/src/pages-new/admin/UserManagement.jsx
+++ client/src/pages-new/admin/UserManagement.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import UsersForm from "../../components-new/user-management/UsersForm";
+import UsersList from "../../components-new/user-management/UsersList";
 
 const UserManagement = () => {
   return (
     <div>
       <h1>User Management</h1>
       <UsersForm />
-      {/* <UsersList /> */}
+      <UsersList />
     </div>
   );
 };
