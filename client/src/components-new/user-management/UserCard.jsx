32e8c47b5bcecc7603eddeaac2b4e90357d22501
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-10-05 23:30:35 -0400

    Add: Modal for rendering EditUserForm

--- client/src/components-new/user-management/UserCard.jsx
+++ client/src/components-new/user-management/UserCard.jsx
@@ -1,7 +1,17 @@
 import React, { useState } from "react";
 import ListGroup from "react-bootstrap/ListGroup";
+import EditUserForm from "./EditUserForm";
+import Modal from "react-bootstrap/Modal";
 
 const UserCard = ({ user }) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
   return (
     <>
       <ListGroup horizontal>
@@ -9,10 +19,18 @@ const UserCard = ({ user }) => {
         <ListGroup.Item>{user.email}</ListGroup.Item>
         <ListGroup.Item>{user.roleName}</ListGroup.Item>
         <ListGroup.Item>
-          <button >Edit</button>
-          <button >Delete</button>
+          <button onClick={handleShow}>Edit</button>
+          <button>Delete</button>
         </ListGroup.Item>
       </ListGroup>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit User</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <EditUserForm user={user} cancelEdit={handleClose} />
+        </Modal.Body>
+      </Modal>
     </>
   );
 };
