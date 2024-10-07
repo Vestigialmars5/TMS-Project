b958855d614f88dbdf247e6612d082fd26d90a2d
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-10-07 17:55:30 -0400

    Refactor: Implement useOnboarding
    - Extracted every state from the object because of validations and errors
    - Made the email immutable
    - Added error display
    - Changed phone pattern
    - Added conditional rendering of the button

--- client/src/components-new/onboarding/UserDetailsForm.jsx
+++ client/src/components-new/onboarding/UserDetailsForm.jsx
@@ -1,40 +1,122 @@
 import React, { useState } from "react";
 import { Form, Spinner } from "react-bootstrap";
+import { useAuth } from "../../hooks/useAuth";
 import { useOnboarding } from "../../hooks/useOnboarding";
+import Button from "react-bootstrap/Button";
 
 const UserDetailsForm = () => {
-  const { submitDetails, status } = useOnboarding();
-  const [userData, setUserData] = useState({
-    email: user.email,
-    password: "",
-    confirmation: "",
-    firstName: "",
-    lastName: "",
-    phoneNumber: "",
-    address: "",
-  });
+  const { user } = useAuth();
+  const { submitUserDetails, submitUserDetailsStatus } = useOnboarding();
+  const email = user.email;
+  const [password, setPassword] = useState("");
+  const [confirmation, setConfirmation] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [address, setAddress] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [confirmationError, setConfirmationError] = useState("");
+  const [firstNameError, setFirstNameError] = useState("");
+  const [lastNameError, setLastNameError] = useState("");
+  const [phoneNumberError, setPhoneNumberError] = useState("");
+  const [addressError, setAddressError] = useState("");
 
-  const handleChange = (e) => {
-    setUserData({ ...userData, [e.target.name]: e.target.value });
+  // TODO: Move validations to a separate file
+
+  const validatePassword = (password) => {
+    if (!password) {
+      return "Password is required";
+    } else if (password.length < 8) {
+      return "Password must be at least 8 characters";
+    }
+    return "";
+  };
+
+  const validateConfirmation = (confirmation) => {
+    // pass
+
+  };
+
+  const validateFirstName = (firstName) => {
+    // pass
+  };
+
+  const validateLastName = (lastName) => {
+    // pass
+  };
+
+  const validatePhoneNumber = (phoneNumber) => {
+    // pass
+  };
+
+  const validateAddress = (address) => {
+    // pass
+  };
+
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+    setPasswordError(validatePassword(e.target.value));
+  };
+
+  const handleConfirmationChange = (e) => {
+    setConfirmation(e.target.value);
+    setConfirmationError(validateConfirmation(e.target.value));
+  };
+
+  const handleFirstNameChange = (e) => {
+    setFirstName(e.target.value);
+    setFirstNameError(validateFirstName(e.target.value));
+  };
+
+  const handleLastNameChange = (e) => {
+    setLastName(e.target.value);
+    setLastNameError(validateLastName(e.target.value));
+  };
+
+  const handlePhoneNumberChange = (e) => {
+    setPhoneNumber(e.target.value);
+    setPhoneNumberError(validatePhoneNumber(e.target.value));
+  };
+
+  const handleAddressChange = (e) => {
+    setAddress(e.target.value);
+    setAddressError(validateAddress(e.target.value));
   };
 
   const handleSubmit = (e) => {
-    // TODO: Validations
     e.preventDefault();
-    submitDetails(userData);
-  };
+    const passwordErr = validatePassword(password);
+    const confirmationErr = validateConfirmation(confirmation);
+    const firstNameErr = validateFirstName(firstName);
+    const lastNameErr = validateLastName(lastName);
+    const phoneNumberErr = validatePhoneNumber(phoneNumber);
+    const addressErr = validateAddress(address);
 
+    if (
+      passwordErr ||
+      confirmationErr ||
+      firstNameErr ||
+      lastNameErr ||
+      phoneNumberErr ||
+      addressErr
+    ) {
+      setPasswordError(passwordErr);
+      setConfirmationError(confirmationErr);
+      setFirstNameError(firstNameErr);
+      setLastNameError(lastNameErr);
+      setPhoneNumberError(phoneNumberErr);
+      setAddressError(addressErr);
+    } else {
+      submitUserDetails({ email, password, confirmation, firstName, lastName, phoneNumber, address });
+    }
+  };
 
   return (
     <Form onSubmit={handleSubmit}>
       <Form.Group controlId="email">
         <Form.Label>Email</Form.Label>
-        <Form.Control
-          type="email"
-          name="email"
-          value={userData.email}
-          readOnly
-        />
+        <Form.Control type="email" name="email" value={email} disabled readOnly />
       </Form.Group>
 
       <Form.Group controlId="password">
@@ -42,67 +124,78 @@ const UserDetailsForm = () => {
         <Form.Control
           type="password"
           name="password"
-          value={userData.password}
-          onChange={handleChange}
+          value={password}
+          onChange={handlePasswordChange}
         />
       </Form.Group>
+      {passwordError && <p>{passwordError}</p>}
 
       <Form.Group controlId="confirmation">
         <Form.Label>Confirm Password</Form.Label>
         <Form.Control
           type="password"
           name="confirmation"
-          value={userData.confirmation}
-          onChange={handleChange}
+          value={confirmation}
+          onChange={handleConfirmationChange}
         />
       </Form.Group>
+      {confirmationError && <p>{confirmationError}</p>}
+
       <Form.Group controlId="firstName">
         <Form.Label>First Name</Form.Label>
         <Form.Control
           type="text"
           name="firstName"
-          value={userData.firstName}
-          onChange={handleChange}
+          value={firstName}
+          onChange={handleFirstNameChange}
         />
       </Form.Group>
+      {firstNameError && <p>{firstNameError}</p>}
 
       <Form.Group controlId="lastName">
         <Form.Label>Last Name</Form.Label>
         <Form.Control
           type="text"
           name="lastName"
-          value={userData.lastName}
-          onChange={handleChange}
+          value={lastName}
+          onChange={handleLastNameChange}
         />
       </Form.Group>
+      {lastNameError && <p>{lastNameError}</p>}
 
       <Form.Group controlId="phoneNumber">
         <Form.Label>Phone Number</Form.Label>
         <Form.Control
           type="tel"
           name="phoneNumber"
-          pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-          value={userData.phoneNumber}
-          onChange={handleChange}
+          pattern="[0-9]{3}[0-9]{3}[0-9]{4}"
+          value={phoneNumber}
+          onChange={handlePhoneNumberChange}
         />
       </Form.Group>
+      {phoneNumberError && <p>{phoneNumberError}</p>}
 
       <Form.Group controlId="address">
         <Form.Label>Address</Form.Label>
         <Form.Control
           type="text"
           name="address"
-          value={userData.address}
-          onChange={handleChange}
+          value={address}
+          onChange={handleAddressChange}
         />
       </Form.Group>
+      {addressError && <p>{addressError}</p>}
 
-      {status !== "loading" ? (
-        <button type="submit" disabled={status === "loading"}>
-          Login
-        </button>
+      {submitUserDetailsStatus !== "loading" ? (
+        <Button
+          type="submit"
+          variant="primary"
+          disabled={submitUserDetailsStatus === "loading"}
+        >
+          Submit
+        </Button>
       ) : (
-        <Spinner animation="border" role="status" />
+        <Spinner animation="border" role="submitDetailsStatus" /> // TODO: Check other appearances of this case, standardize if spinner inside button or instead of
       )}
     </Form>
   );
