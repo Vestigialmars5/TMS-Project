456b05ebb54b2042b2b456d41bd09b4179355990
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-10-07 17:56:34 -0400

    Refactor: Change to use Mutation and react-query

--- client/src/hooks/useOnboarding.js
+++ client/src/hooks/useOnboarding.js
@@ -1,25 +1,44 @@
-import { useRoleBasedNavigation } from "./useRoleBasedNavigation";
-import { useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import tokenService from "../services/tokenService";
+import {
+  setUser,
+  startAuthenticating,
+  stopAuthenticating,
+} from "../store/slices/authSlice";
+import { useDispatch } from "react-redux";
+import { showAlert } from "../store/actions/alertsActions";
 import * as onboardingService from "../services/onboardingService";
 
 export const useOnboarding = () => {
-  const [status, setStatus] = useState("idle");
-  const { goToDashboard } = useRoleBasedNavigation();
+  const dispatch = useDispatch();
+  const queryClient = useQueryClient();
 
-  const submitDetails = async (details) => {
-    setStatus("loading");
-    try {
-      const response = await onboardingService.submitUserDetails(details);
-      goToDashboard(response.roleId);
-    } catch (error) {
-      // pass
-    } finally {
-      setStatus("idle");
-    }
-  };
+  const submitUserDetailsMutation = useMutation({
+    mutationFn: (details) => {
+      dispatch(startAuthenticating()); // Set user stops authenticating automatically
+      return onboardingService.submitUserDetails(details);
+    },
+    onSuccess: (data) => {
+      tokenService.setAccessToken(data.accessToken);
+      queryClient.setQueryData("user", data.user);
+      dispatch(setUser(data.user));
+      showAlert("User Details Submitted", "success");
+    },
+    onError: (error) => {
+      const message =
+        error.response?.data?.description ||
+        error.response?.data?.error ||
+        "An Unknown Error Occurred";
+      showAlert(message, "danger");
+      dispatch(stopAuthenticating());
+    },
+  });
+
+  const submitUserDetails = (details) =>
+    submitUserDetailsMutation.mutate(details);
 
   return {
-    submitDetails,
-    status,
+    submitUserDetails,
+    submitUserDetailsStatus: submitUserDetailsMutation.status,
   };
 };
