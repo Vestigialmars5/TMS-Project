ac379de50eafaf4af8393f346d9e7e0f5c0e4b43
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-10-09 21:58:20 -0400

    Refactor: home, layouts
    - Removed CustomRoute.jsx handled by routes
    - Removed .css

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import LogoutButton from "../../components-new/auth/LogoutButton";
+import LogoutButton from "../../components/auth/LogoutButton";
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
