0357a51d7646205b5b31000889c48aacf45991a9
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-10-14 21:09:08 -0400

    Refactor: decorator to work without jwt claims
    - Now queries db for role name

--- server/utils/authorization_decorators.py
+++ server/utils/authorization_decorators.py
@@ -1,8 +1,12 @@
 from flask import jsonify, abort
 from functools import wraps
+from extensions import db
+from models.tms_models import User
 
-from flask_jwt_extended import get_jwt
+from flask_jwt_extended import get_jwt_identity
+from server.utils.exceptions import DatabaseQueryError
 from jwt.exceptions import InvalidTokenError
+from sqlalchemy.exc import SQLAlchemyError
 
 # Decorator for roles required
 
@@ -12,9 +16,18 @@ def roles_required(*roles):
         @wraps(f)
         def decorated_function(*args, **kwargs):
             try:
-                current_user_role = get_jwt()["roleName"]
-            except Exception as e:
-                raise e
+                user_id = get_jwt_identity()
+                user = db.session.query(User).filter_by(id=user_id).first()
+                
+            except SQLAlchemyError as e:
+                raise DatabaseQueryError("Error Retrieving User Role")
+            except Exception:
+                raise
+
+            if not user:
+                abort(404, description="User Not Found")
+
+            current_user_role = user.role.role_name
 
             if current_user_role not in roles:
                 roles_clean = ", ".join(roles)
