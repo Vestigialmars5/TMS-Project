3476cf8745128e4fb77e4d4537ab224cf9bbea13
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-10-17 23:24:49 -0400

    Fix: formatting

--- client/src/services/onboardingService.js
+++ client/src/services/onboardingService.js
@@ -1,7 +1,6 @@
 import api from "./apiService";
 
-
 export const submitUserDetails = async (details) => {
   const response = await api.post("/onboarding/details", details);
   return response.data;
-}
+};
