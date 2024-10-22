ba687c63ca078eff54893fca3b8056bcab1d5e96
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-10-22 19:44:46 -0400

    Fix: Delete unused import

--- client/src/components/common/NavBar.jsx
+++ client/src/components/common/NavBar.jsx
@@ -2,7 +2,6 @@ import { Link, useMatch } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import { useRoleBasedNavigation } from "../../hooks/useRoleBasedNavigation";
 import { useSelector } from "react-redux";
 import { useAuth } from "../../hooks/useAuth";
