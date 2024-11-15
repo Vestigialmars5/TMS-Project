3c7f168aaa0e2798e43e6233a6a24a3ac5450fab
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2024-11-14 20:02:35 -0500

    Add: Create components for list and editing

--- client/src/pages/customer/OrderManagement.jsx
+++ client/src/pages/customer/OrderManagement.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import OrderForm from "../../components/orders/CreateOrder/OrderForm";
+import OrdersList from "../../components/orders/OrderList/OrdersList";
 
 const CustomerDashboard = () => {
   return (
     <div>
       <h1>Order Management</h1>
       <OrderForm />
+      <OrdersList />
     </div>
   );
 };
