f7bb46b64b8f9b10b9721e105dcbd5a7efcdae89
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2025-04-15 19:04:26 -0400

    Add: dispatch actions from the orders slice

--- client/src/hooks/useOrders.js
+++ client/src/hooks/useOrders.js
@@ -1,14 +1,18 @@
+import { useDispatch } from "react-redux";
+import { setOrders, addOrder, removeOrder } from "../store/slices/ordersSlice";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import * as orderService from "../services/orderService";
 import { showAlert } from "../store/actions/alertsActions";
 
 export const useOrders = () => {
   const queryClient = useQueryClient();
+  const dispatch = useDispatch();
 
   const createOrderMutation = useMutation({
     mutationFn: (order) => orderService.createOrder(order),
     onSuccess: () => {
       queryClient.invalidateQueries("orders");
+      dispatch(addOrder(order));
       showAlert("Order Created Successfully", "success");
     },
     onError: (error) => {
