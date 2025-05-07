7c891b517397485a7bcb8319d30dfc9f1afceb62
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2025-05-07 19:23:39 -0400

    Add: skip button for roles without forms
    - This is to be able to skip the onboarding step and set the status as active

--- client/src/pages/onboarding/Onboarding.jsx
+++ client/src/pages/onboarding/Onboarding.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import UserDetailsForm from "../../components/onboarding/UserDetailsForm";
 import CustomerDetailsForm from "../../components/onboarding/CustomerDetailsForm";
 import { useQuery } from "@tanstack/react-query";
@@ -6,9 +6,12 @@ import Spinner from "react-bootstrap/Spinner";
 import { useAuth } from "../../hooks/useAuth";
 import { getCurrentStep } from "../../services/onboardingService";
 import { showAlert } from "../../store/actions/alertsActions";
+import { useOnboarding } from "../../hooks/useOnboarding";
+import Button from "react-bootstrap/Button";
 
 const Onboarding = () => {
   const { user } = useAuth();
+  const {submitRoleDetails} = useOnboarding();
   const {data: currentStep, status: currentStepStatus, error} = useQuery({
     queryKey: ["currentStep"],
     queryFn: () => getCurrentStep(),
@@ -33,6 +36,12 @@ const Onboarding = () => {
     4: CustomerDetailsForm,
   };
 
+  const PlaceholderStep = () => {
+    const roleId = user.roleId
+    const details = {roleId, roleId}
+    submitRoleDetails({ roleId, details});
+  }
+
   const STEP_COMPONENTS = {
     1: UserDetailsForm,
     2: roleForms[user.roleId] || (() => <p>Role-specific form not found</p>),
@@ -43,6 +52,13 @@ const Onboarding = () => {
       <h1>Onboarding</h1>
       {STEP_COMPONENTS[currentStep] &&
         React.createElement(STEP_COMPONENTS[currentStep])}
+      {roleForms[user.roleId] ? (
+        <></>
+      ) : (
+        <Button type="submit" onClick={PlaceholderStep}>
+          Skip
+        </Button>
+      )}
     </div>
   );
 
